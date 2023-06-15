f656f38076170118f46ecc54b52d114858c1e610
Mohit-0309 <125802864+Mohit-0309@users.noreply.github.com>
2023-06-15 19:16:53 +0530

    Add files via upload

---
+++ l7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+bool isPalindrome(int x){
+    if(x>=-2^31 && x<=2^31-1)
+    {
+        int n=x;
+        long int sum=0;
+        while(n!=0)
+        {
+            int d=n%10;
+            sum=sum*10+d;
+            n=n/10;
+        }
+        if(x<0)
+        {
+            return false;
+        }
+        if(sum==x)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
